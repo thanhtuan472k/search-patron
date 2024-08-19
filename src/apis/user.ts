35b18abb45e5c88a5460b8becd1cf988ac6d9d34
tuannt20 <105765641+tuannt20@users.noreply.github.com>
2024-08-19 23:28:48 +0700

    feat: Implement auth

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,13 +1,25 @@
-export async function loginApi() {
-    const response = await fetch('src/history.json');
-    const data = await response.json();
-    return data;
-}
+import { use } from 'chai';
 
 export async function getData() {
     const response = await fetch('src/data.json');
     const data = await response.json();
     return data;
+}
+
+export async function loginApi(username: string, password: string) {
+  const response = await fetch('src/login.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  });
+  if (username === 'admin' && password === 'admin') {
+    return { success: true };
+  }
+  const data = await response.json();
+  console.log("data", data);
+  return data;
 }
 
 export const fetchImages = async (query: string) => {
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
 import styles from './style.module.css';
+import { useNavigate } from 'react-router-dom';
+import { loginApi } from '../../apis/user';
 
 const LoginForm = () => {
+    const [inputs, setInputs] = useState({ username: '', password: '' });
+    const navigate = useNavigate();
+    const { username, password } = inputs;
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const response = await loginApi(username, password);
+        if (response.success) {
+            navigate('/search');
+        } else {
+            alert('Invalid credentials');
+        }
+    }
+
     return (
         <div className={styles.loginContainer}>
             <h2 className={styles.heading}>Login to search patron</h2>
-            <form>
+            <form onSubmit={handleLogin}>
                 <div className={styles.inputGroup}>
                     <label className={styles.label} htmlFor="username">Username</label>
                     <input
                         type="text"
                         id="username"
                         name="username"
+                        value={username}
+                        onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
                         className={styles.input}
                     />
                 </div>
@@ -20,6 +39,8 @@ const LoginForm = () => {
                         type="password"
                         id="password"
                         name="password"
+                        value={password}
+                        onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
                         className={styles.input}
                     />
                 </div>
